b25b9c838cc686cab98ba042d2c82addc9410f7e
camcam <cam.toulouse@yahoo.fr>
2020-03-20 15:46:45 +0100

    ajout bouton espace admin

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface as EMI;
 use App\Entity\Message;
 use App\Form\MessageType;
 use App\Repository\MessageRepository;
+use App\Repository\CommentaireRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class MessageController extends AbstractController
@@ -41,11 +42,12 @@ class MessageController extends AbstractController
      * @Route("/admin/message/list", name="message_list")
      * * @IsGranted("ROLE_ADMIN")
      */
-    public function affichageMessages(MessageRepository $mr)
+    public function affichageMessages(MessageRepository $mr, CommentaireRepository $cr)
     {      
         $messages = $mr->findAll();
+        $commentaires = $cr->findAll();
 
-        return $this->render('user/admin.html.twig', compact("messages"));
+        return $this->render('user/admin.html.twig', compact("messages", "commentaires"));
        
     }
 
@@ -69,5 +71,17 @@ class MessageController extends AbstractController
         $formMessage = $formMessage->createView();
         return $this->render('message/formulaire.html.twig', compact("formMessage", "bouton"));
 
+    }
+
+    /**
+     * @Route("/admin/commentaire/supprimer/{id}", name="comment_delete")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteCom(CommentaireRepository $cr, Request $request,EMI $em, int $id)
+    {
+        $messageAsupprimer = $cr->find($id);
+        $em->remove($messageAsupprimer);
+        $em->flush();
+        return $this->redirectToRoute("message_list"); 
     }
 }
