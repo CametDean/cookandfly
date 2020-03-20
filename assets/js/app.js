a20adf2058f5b1d8112b78eecb4eaefab585b19d
Thanaya Name <thanaya.rampersan@gmail.com>
2020-03-20 15:59:00 +0100

    animationjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,26 @@ require("bootstrap")
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+
+
+
+//Animer le nom des continents pages continents & le nom des recettes
+
+  var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml16',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
